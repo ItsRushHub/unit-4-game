58545815cbec95c34473effffbf665cb6c2aa86d
Oran Rush <oranrush@Orans-MBP.lan1>
2018-12-21 11:35:27 -0600

    Upload6

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,37 +17,44 @@ var yourTotalScoreIs = [];
 // Set each crystal to have a random hidden value between 1 - 12
 // when the game begins
 
-
-
-for (var i = 0; i < 4; i++){
-    
-    var crystals = Math.floor(Math.random() * 11) + 1;
-
-    console.log(crystals);
-}
-
 // On the clicked crystal add to the previous number 
 // until it equals the total score
-$(".crystals").on('click', function () {
-    console.log($(this));
+
+$(function (){
+
+        $(".crystal1").on("click",function() {
+            var crystal1 = Math.floor(Math.random() * 11) + 1;
+            console.log(crystal1);
+        });
+
+        $(function () {
+            $(".crystal2").on("click",function() {
+                var crystal2 = Math.floor(Math.random() * 11) + 1;
+                console.log(crystal2);
+            });
+        });
+
+        $(function () {
+            $(".crystal3").on("click",function() {
+                var crystal3 = Math.floor(Math.random() * 11) + 1;
+                console.log(crystal3);
+            });
+        });
+
+        $(function () {
+            $(".crystal4").on("click",function() {
+                var crystal4 = Math.floor(Math.random() * 11) + 1;
+                console.log(crystal4);
+            });
+        });
 });
 
 
-
 // Set new random number for each win or lose on each crystal
-
-
-
-
-
-
-
-
 // If it equals increase wins by one & start over
-
-
-
 // If it doesn't equal increase losesses by one and start game over
+
+
 
 
 
